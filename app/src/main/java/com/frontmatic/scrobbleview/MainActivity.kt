75dce9f9e4b7e6bb6a8daa297ff6dd9fb8a7183a
Vujke <vladimirvuj@gmail.com>
2023-07-17 15:01:42 +0200

    cleanup

--- app/src/main/java/com/frontmatic/scrobbleview/MainActivity.kt
+++ app/src/main/java/com/frontmatic/scrobbleview/MainActivity.kt
@@ -7,10 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.compose.rememberNavController
 import com.frontmatic.scrobbleview.ui.components.BottomNavBar
 import com.frontmatic.scrobbleview.ui.components.RootScaffold
@@ -53,7 +50,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
                         DestinationsNavHost(
                             navController = navController,
                             navGraph = NavGraphs.root
